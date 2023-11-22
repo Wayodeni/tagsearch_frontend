bcbe11faf552066b57cb5ab3d8ecc8e3ad5edf15
lev sviridov <levgeneral@yandex.ru>
2023-11-23 00:41:01 +0300

    added tag selection changed documents margin

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ import Typography from '@mui/material/Typography';
 function App() {
   const [tags, setTags] = useState([]);
   const [documents, setDocuments] = useState([]);
-  const [searchBar, setSearchBar] = useState("");
-  const [selectedTags, setSelectedTags] = useState([]);
+
+  const emptyTagNamePlaceholder = "Без тегов"
+
+  useEffect(() => fetchSearchResults(""), [])
 
   const fetchSearchResults = (query) => {
     fetch(`http://localhost:8080/api/v1/search?query=${query}`, {
@@ -29,12 +31,28 @@ function App() {
       })
   }
 
-  useEffect(() => fetchSearchResults(""), [])
-
   const handleSearchbarChange = (query) => {
-    setSearchBar(query);
     fetchSearchResults(query);
     console.log(query);
+  }
+
+  const getTagName = (tagContent) => tagContent.split(' ').slice(0, -1).join(' ')
+
+  const handleTagClick = (tagContent) => {
+    let tagName = getTagName(tagContent)
+    let indexOfCurrentTag = tags.findIndex(stateTag => stateTag.name === tagName)
+    if (tagName === emptyTagNamePlaceholder) {
+      indexOfCurrentTag = tags.findIndex(stateTag => stateTag.name === '')
+    }
+    console.log(indexOfCurrentTag, tagContent)
+    setTags([
+      ...tags.slice(0, indexOfCurrentTag),
+      {
+        ...tags[indexOfCurrentTag],
+        selected: true,
+      },
+      ...tags.slice(indexOfCurrentTag + 1),
+    ])
   }
 
   return (
@@ -50,19 +68,19 @@ function App() {
     <Box sx={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'flex-start'}}>
       {tags?.map((tag) => 
         <>
-          <Chip key={tag.id} label={`${tag.name ? tag.name : ("Без тегов")} (${tag.documentCount})`}></Chip>
+          <Chip key={tag.id} color={tag.selected ? 'success' : 'default'} label={`${tag.name ? tag.name : (emptyTagNamePlaceholder)} (${tag.documentCount})`} onClick={e => handleTagClick(e.target.innerText)}></Chip>
         </>
       )}
     </Box>
     <Box sx={{display: 'flex', flexDirection: 'column'}}>
       {documents?.map((document) => 
-        <>
-          <Chip key={document.id} label={`${document.name ? document.name : ("Документ без названия")}`} size="small" variant="outlined"/>
+        <Box sx={{mt: 1}}>
+          <Chip  key={document.id} label={`${document.name ? document.name : ("Документ без названия")}`} size="small" variant="outlined"/>
           <Typography align={'justify'}>{document.body}</Typography>
           <Box sx={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
             {document.tags?.map((tag) => <Chip label={tag.name + ` (${tags.find(stateTag => stateTag.name == tag.name)?.documentCount})`} size="small" />)}
           </Box>
-        </>
+        </Box>
       )}
     </Box>
     
