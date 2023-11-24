435329e0a6a2552ac59146f1df99787cb5483a20
Lev Sviridov <levgeneral@yandex.ru>
2023-11-24 15:00:20 +0300

    autoformat changed getting tag properties from innerText to mappend obj

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 import { useState, useContext, useEffect } from "react";
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 
 function App() {
   const [tags, setTags] = useState([]);
   const [documents, setDocuments] = useState([]);
 
-  const emptyTagNamePlaceholder = "Без тегов"
+  const emptyTagNamePlaceholder = "Без тегов";
 
-  useEffect(() => fetchSearchResults(""), [])
+  useEffect(() => fetchSearchResults(""), []);
 
   const fetchSearchResults = (query) => {
     fetch(`http://localhost:8080/api/v1/search?query=${query}`, {
@@ -21,82 +21,132 @@ function App() {
     })
       .then((response) => {
         if (response.ok) {
-          return response.json()
+          return response.json();
         }
-        throw response
+        throw response;
       })
       .then((searchResponse) => {
-        updateTags(searchResponse.tags?.sort((a, b) => b.documentCount - a.documentCount))
-        setDocuments(searchResponse.documents)
-      })
-  }
+        updateTags(
+          searchResponse.tags?.sort((a, b) => b.documentCount - a.documentCount)
+        );
+        setDocuments(searchResponse.documents);
+      });
+  };
 
   const updateTags = (foundTags) => {
-    let selectedTagNames = tags?.map(tag => tag.selected ? tag.name : undefined).filter(tag => tag)
-    setTags(foundTags?.map(tag => selectedTagNames?.includes(tag.name) ? {...tag, selected: true} : tag))
-  }
+    let selectedTagNames = tags
+      ?.map((tag) => (tag.selected ? tag.name : undefined))
+      .filter((tag) => tag);
+    setTags(
+      foundTags?.map((tag) =>
+        selectedTagNames?.includes(tag.name) ? { ...tag, selected: true } : tag
+      )
+    );
+  };
 
-  const handleSearchbarChange = (query) => {
-    fetchSearchResults(query);
-    console.log(query);
-  }
+  const handleTagClick = (tag) => {
+    let tagName = tag.name;
+    let indexOfCurrentTag = tags.findIndex(
+      (stateTag) => stateTag.name === tagName
+    );
 
-  const getTagName = (tagContent) => tagContent.split(' ').slice(0, -1).join(' ')
-
-  const handleTagClick = (tagContent) => {
-    let tagName = getTagName(tagContent)
-    let indexOfCurrentTag = tags.findIndex(stateTag => stateTag.name === tagName)
-    if (tagName === emptyTagNamePlaceholder) {
-      indexOfCurrentTag = tags.findIndex(stateTag => stateTag.name === '')
-    }
     const getUpdatedTag = () => {
       if (tags[indexOfCurrentTag].selected) {
         return {
           ...tags[indexOfCurrentTag],
           selected: false,
-        }
+        };
       }
       return {
         ...tags[indexOfCurrentTag],
         selected: true,
-      }
-    }
+      };
+    };
     setTags([
       ...tags.slice(0, indexOfCurrentTag),
       getUpdatedTag(),
       ...tags.slice(indexOfCurrentTag + 1),
-    ])
-  }
+    ]);
+  };
 
   return (
     <>
-    <TextField 
-      id="outlined-basic" 
-      label="Поисковый запрос" 
-      variant="outlined" 
-      onChange={(e) => handleSearchbarChange(e.target.value)}
-      sx={{width: '100%'}}
-    />
-    <Typography>Найдено документов: {documents?.length} Тегов: {tags?.length}</Typography>
-    <Box sx={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'flex-start'}}>
-      {tags?.map((tag) => 
-        <>
-          <Chip key={tag.id} color={tag.selected ? 'success' : 'default'} label={`${tag.name ? tag.name : (emptyTagNamePlaceholder)} (${tag.documentCount})`} onClick={e => handleTagClick(e.target.innerText)}></Chip>
-        </>
-      )}
-    </Box>
-    <Box sx={{display: 'flex', flexDirection: 'column'}}>
-      {documents?.map((document) => 
-        <Box sx={{mt: 1}}>
-          <Chip  key={document.id} label={`${document.name ? document.name : ("Документ без названия")}`} size="small" variant="outlined"/>
-          <Typography align={'justify'}>{document.body}</Typography>
-          <Box sx={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
-            {document.tags?.map((tag) => <Chip onClick={e => handleTagClick(e.target.innerText)} color={tags.find(stateTag => stateTag.name === tag.name)?.selected ? 'success' : 'default'} label={tag.name + ` (${tags.find(stateTag => stateTag.name == tag.name)?.documentCount})`} size="small" />)}
+      <TextField
+        id="outlined-basic"
+        label="Поисковый запрос"
+        variant="outlined"
+        onChange={(e) => fetchSearchResults(e.target.value)}
+        sx={{ width: "100%" }}
+      />
+      <Typography>
+        Найдено документов: {documents?.length} Тегов: {tags?.length}
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+        }}
+      >
+        {tags?.map((tag) => (
+          <>
+            <Chip
+              key={tag.id}
+              color={tag.selected ? "success" : "default"}
+              label={`${tag.name ? tag.name : emptyTagNamePlaceholder} (${
+                tag.documentCount
+              })`}
+              onClick={() => handleTagClick(tag)}
+            ></Chip>
+          </>
+        ))}
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        {documents?.map((document) => (
+          <Box sx={{ mt: 1 }}>
+            <Chip
+              key={document.id}
+              label={`${
+                document.name ? document.name : "Документ без названия"
+              }`}
+              size="small"
+              variant="outlined"
+            />
+            <Typography align={"justify"}>{document.body}</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+              }}
+            >
+              {document.tags?.map((tag) => (
+                <Chip
+                  onClick={() => handleTagClick(tag)}
+                  color={
+                    tags.find((stateTag) => stateTag.name === tag.name)
+                      ?.selected
+                      ? "success"
+                      : "default"
+                  }
+                  label={
+                    tag.name +
+                    ` (${
+                      tags.find((stateTag) => stateTag.name == tag.name)
+                        ?.documentCount
+                    })`
+                  }
+                  size="small"
+                />
+              ))}
+            </Box>
           </Box>
-        </Box>
-      )}
-    </Box>
-    
+        ))}
+      </Box>
     </>
   );
 }
